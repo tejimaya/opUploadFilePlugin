32c51599ece5cb37f12efa30ba35c4afe38c39d8
Kimura Youichi <kim.upsilon@bucyou.net>
2012-09-05 16:47:09 +0900

    アップロード画面のUIを修正

--- apps/pc_frontend/modules/dropbox/templates/_dropboxMenu.php
+++ apps/pc_frontend/modules/dropbox/templates/_dropboxMenu.php
@@ -10,7 +10,11 @@
 
 <script id="dropbox-menuitem-template" type="text/x-jquery-tmpl">
 {{each data.contents}}
-<li class="dropbox-item"><a class="shareLink" href="<?php echo $sf_request->getRelativeUrlRoot() ?>/f/show${path}">${name}</a></li>
+<li class="dropbox-item">
+  <a href="<?php echo $sf_request->getRelativeUrlRoot() ?>/f/show${path}">
+    ${name}
+  </a>
+</li>
 {{/each}}
 </script>
 
@@ -28,6 +32,11 @@ $('#dropbox-menu .dropdown-toggle').click(function(){
       $('#dropbox-menuitems .dropbox-item').remove();
 
       var menuitem = $('#dropbox-menuitem-template').tmpl(json);
+
+      $('a', menuitem).click(function(event){
+        if ($.inArray(event.target.tagName, ['BUTTON', 'I']) !== -1) event.preventDefault();
+      });
+
       $('#dropbox-menuitems').append(menuitem);
     }
   );
@@ -44,23 +53,22 @@ $(function(){
     <h3 id="dropbox-uploadmodal-label">アップロード</h3>
   </div>
   <div class="modal-body" id="dropbox-uploadmodal-body1">
-    <label>ファイル: <input name="upfile" type="file" id="dropbox-uploadmodal-upfile"></label>
+    ファイル: <input name="upfile" type="file" id="dropbox-uploadmodal-upfile">
+    <?php echo op_image_tag('ajax-loader.gif', array('class' => 'hide', 'id' => 'dropbox-uploadsubmit-loading')) ?>
   </div>
   <div class="modal-body hide" id="dropbox-uploadmodal-body2">
-    URL: <a id="dropbox-upload-url"></a>
+    <span id="dropbox-upload-url"></span>
   </div>
   <div class="modal-footer">
-    <button class="btn" data-dismiss="modal">閉じる</button>
     <button class="btn btn-primary" id="dropbox-uploadsubmit">
-      <span id="dropbox-uploadsubmit-text">実行</span>
-      <?php echo op_image_tag('ajax-loader.gif', array('class' => 'hide', 'id' => 'dropbox-uploadsubmit-loading')) ?>
+      <span id="dropbox-uploadsubmit-text">アップロード</span>
     </button>
   </div>
 </div>
 
 <script>
 $('#dropbox-uploadsubmit').click(function(){
-  $('#dropbox-uploadsubmit-text').hide();
+  $('#dropbox-uploadsubmit').attr('disabled', 'disabled');
   $('#dropbox-uploadsubmit-loading').show();
 
   $('#dropbox-uploadmodal .modal-body').upload(
@@ -76,10 +84,10 @@ $('#dropbox-uploadsubmit').click(function(){
 
       if (json.status !== 'success') throw 'dropbox/upload failed.';
 
-      var url = '<?php echo url_for('dropbox/index', array('absolute' => true)) ?>?path=' +
-        encodeURIComponent('/m<?php echo $member->getId() ?>/' + $('#dropbox-uploadmodal-upfile').val());
+      var url = '<?php echo $sf_request->getUriPrefix().$sf_request->getRelativeUrlRoot() ?>/f/show' +
+        '/m<?php echo $member->getId() ?>/' + $('#dropbox-uploadmodal-upfile').val();
 
-      $('#dropbox-upload-url').attr('href', url).text(url);
+      $('#dropbox-upload-url').text(url);
 
       $('#dropbox-uploadmodal-body1').hide();
       $('#dropbox-uploadmodal-body2').show();
